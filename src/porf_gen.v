820435c2c74ba7c637114605b005c29a68799158
George Castillo <gmcastil@gmail.com>
2017-04-02 21:01:59 -0600

    Added a power-on reset (POR) block.  This is just a proof of concept I think.  Once I know that the design works, I'm going to automate the construction for an arbitrary number of FDPE with a Python script of some sort.

---
+++ src/porf_gen.v
@@ -0,0 +1,63 @@
+module porf_gen
+  (
+   input  reset_in,
+   input  clk,
+   output reset_out
+   );
+
+  wire    d_1;
+  wire    q_1;
+
+  wire    d_2;
+  wire    q_2;
+
+  wire    d_3;
+  wire    q_3;
+
+  wire    d_4;
+  wire    q_4;
+
+  FDPE #(
+         .INIT(1'b0)
+         ) FDPE_1 (
+                   .Q(q_1),
+                   .C(clk),
+                   .CE(1'b1),
+                   .PRE(reset_in),
+                   .D(1'b0)
+                   );
+
+  FDPE #(
+         .INIT(1'b0)
+         ) FDPE_2 (
+                   .Q(q_2),
+                   .C(clk),
+                   .CE(1'b1),
+                   .PRE(reset_in),
+                   .D(q_1)
+                   );
+
+  FDPE #(
+         .INIT(1'b0)
+         ) FDPE_3 (
+                   .Q(q_3),
+                   .C(clk),
+                   .CE(1'b1),
+                   .PRE(reset_in),
+                   .D(q_2)
+                   );
+
+  FDPE #(
+         .INIT(1'b0)
+         ) FDPE_4 (
+                   .Q(q_4),
+                   .C(clk),
+                   .CE(1'b1),
+                   .PRE(reset_in),
+                   .D(q_3)
+                   );
+
+  assign reset_out = q_4;
+  assign d_1 = 1'b0;
+
+endmodule // porf_gen
---
+++ tests/porf_gen_tb.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module porf_gen_tb();
+
+  reg  clk_100m;
+  reg  reset_in;
+  wire reset_out;
+
+  initial begin
+    clk_100m <= 1'b1;
+      forever begin
+        #5 clk_100m <= ~clk_100m;
+      end
+    end  
+
+
+  porf_gen #(
+    ) u_porf_gen (
+                  .reset_in(reset_in),
+                  .clk(clk_100m),
+                  .reset_out(reset_out));
+
+  initial begin
+    reset_in = 1'b0;
+    #23;
+    reset_in = 1'b1;
+    #54;
+    reset_in = 1'b0;
+    #53;
+  end
+
+endmodule
