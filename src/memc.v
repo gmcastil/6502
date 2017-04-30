7d375dced07cff2cbc67ed37696caa17a4eb044a
George Castillo <gmcastil@gmail.com>
2017-04-29 23:25:14 -0600

    Fleshed out remainder of states for memory controller state machine.  Need to figure out BRAM instantiation and then get the signals, read, and writes straightened out.

--- src/memc.v
+++ src/memc.v
@@ -23,12 +23,12 @@ module memc
   localparam READ       = 8;
   localparam WRITE      = 9;
 
-  reg [4:0]     state;
-  reg [4:0]     next;
+  reg [9:0]     state;
+  reg [9:0]     next;
 
   // --- Signal declarations
-  localparam [7:0] WR_PATT_1 = 8'hAA;
-  localparam [7:0] WR_PATT_2 = 8'h55;
+  localparam [7:0] WR_PATT_1 = 8'b01010101;
+  localparam [7:0] WR_PATT_2 = 8'b10101010;
 
   reg [15:0]    bist_addr;
   reg [15:0]    top_addr;
@@ -43,28 +43,70 @@ module memc
       state[RESET] <= 1'b1;
     end else begin
       state <= next;
-r end
+    end
   end
 
   always @(*) begin
 
-    next = 5'b0;
+    next = 10'b0;
 
     case (1'b1)
 
       state[RESET]: begin
+        next <= BIST;
       end
 
       state[BIST]: begin
+        if (bist_done == 1'b0) begin
+          next <= TEST_WR1;
+        end else begin
+          next <= IDLE;
+        end
+      end
+
+      state[TEST_WR1]: begin
+        next <= TEST_RD1;
+      end
+
+      state[TEST_RD1]: begin
+        if (read_pattern == WR_PATT_1) begin
+          next <= TEST_WR2;
+        end else begin
+          next <= ERROR;
+        end
+      end
+
+      state[TEST_WR2]: begin
+        next <= TEST_RD2;
+      end
+
+      state[TEST_RD2]: begin
+        if (read_pattern == WR_PATT_1) begin
+          next <= BIST;
+        end else begin
+          next <= ERROR;
+        end
+      end
+
+      state[ERROR]: begin
+        next <= ERROR;
       end
 
       state[IDLE]: begin
+        if  (read_en == 1'b1) begin
+          next <= READ;
+        end else if (write_en = 1'b1) begin
+          next <= WRITE;
+        end else begin
+          next <= IDLE;
       end
 
       state[READ]: begin
+        next <= IDLE;
       end
 
       state[WRITE]: begin
+        next <= IDLE;
       end
 
       default: begin
@@ -73,6 +115,6 @@ r end
     endcase // case (1'b1)
   end
 
-  // Instantiate a 64k x 8 BRMA
+  // Instantiate a 64k x 8 BRAM
 
 endmodule // memc
