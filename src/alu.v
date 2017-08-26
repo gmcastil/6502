608a25932149622fb9e3ab2f5b635a1a37653ec3
George Castillo <gmcastil@gmail.com>
2017-08-25 23:01:42 -0600

    Fixed a possible bug in the ALU with width mismatches.

--- src/alu.v
+++ src/alu.v
@@ -7,6 +7,21 @@
 // Description: Early cut at an arithmetic logic unit (ALU) for the
 // 6502 processor. Does not completely support BCD mode yet and has
 // had virtually zero testing performed on it.
+//
+// Usage: The ALU is used by the processor to perform the majority of its
+// arithmetic functions.  Some details about the implementation follow.
+//
+// Control Signals:
+//   ADD = 3'b000
+//   OR  = 3'b001
+//   XOR = 3'b010
+//   AND = 3'b011
+//   SR  = 3'b100
+//   SL  = 3'b101
+//   SUB = 3'b110
+//
+// Logical operations do not distinguish between the A and B inputs.  Shift
+// operations however only support the A input.
 // ----------------------------------------------------------------------------
 
 module alu
@@ -28,18 +43,17 @@ module alu
 
   always @(*) begin
 
+    // Default for all ALU output flags
+    alu_flags = 8'b0;
+
     case ( alu_ctrl )
 
       ADD: begin
         // Affects Flags: N V Z C
-        if (alu_BCD == 1'b1) begin
-          // BCD addition
-        end else begin
           // Binary addition with carry in
-          result = {1'b0, alu_AI} + {1'b0, alu_BI} + alu_carry;
+          result = {1'b0, alu_AI} + {1'b0, alu_BI} + { 8'b0, alu_carry };
           alu_Y = result[7:0];
           alu_flags[CARRY] = result[8];
-        end
       end
 
       // OR: begin
@@ -81,21 +95,27 @@ module alu
         alu_flags[NEG] = alu_Y[7];
       end
 
-//      SR: begin
-//        // Affects Flags: N Z CO
-//        Y = {CI, AI[7:1]};
-//        // Low bit becomes carry: set if low bit is set; cleared if low bit was
-//        // clear
-//        CO = AI[0];
-//        // Set if result is zero; else cleared
-//        if (Y == 8'b0) begin
-//          Z = 1'b0;
-//        end else begin
-//          Z = 1'b1;
-//        end
-//        // Set if MSB is set; else cleared
-//        N = Y[7];
-//      end
+      SR: begin
+        alu_Y = { 1'b0, alu_AI[7:1] };
+        alu_flags[CARRY] = alu_AI[0];
+        if (alu_Y == 8'b0) begin
+          alu_flags[ZERO] = 1'b1;
+        end else begin
+          alu_flags[ZERO] = 1'b0;
+        end
+        alu_flags[NEG] = alu_Y[7];
+      end
+
+      SL: begin
+        alu_Y = { alu_AI[6:0], 1'b0 };
+        alu_flags[CARRY] = alu_AI[7];
+        if (alu_Y == 8'b0) begin
+          alu_flags[ZERO] = 1'b1;
+        end else begin
+          alu_flags[ZERO] = 1'b0;
+        end
+        alu_flags[NEG] = alu_Y[7];
+      end
 
       default: begin end
 
