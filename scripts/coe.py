902bbf8107e9b95eaf8e7d6caced9be2c3132f60
George Castillo <gmcastil@gmail.com>
2017-09-02 16:35:19 -0600

    Assembling the COE file from pseudo-assembly code was really annoying.

---
+++ scripts/coe.py
@@ -0,0 +1,15 @@
+"""
+Collection of tools for working with Xilinx COE files
+
+"""
+
+def from_coe(filename):
+    """Builds a COE file from a 6502 assembly source file
+
+    Args:
+      filename (str) - Name of source file to read from
+
+    Returns:
+      None
+
+    """
