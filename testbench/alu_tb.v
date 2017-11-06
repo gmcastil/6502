0984812173cb1d7906b9ff888acdb11baa244af0
George Castillo <gmcastil@gmail.com>
2017-11-05 21:08:28 -0700

    Added some regression tests for right shift and the remainder of addition.
    I'm not sure that this is how a testsuite should be constructed, but it is at
    least a start.

--- testbench/alu_tb.v
+++ testbench/alu_tb.v
@@ -34,26 +34,31 @@ module alu_tb ();
                  .alu_overflow  (alu_overflow)
                );
 
-  integer   tests_failed = 0;
-  integer   tests_passed = 0;
+  integer   tests_failed;
+  integer   tests_passed;
   integer   result;
   integer   A;
   integer   B;
 
+  reg       carry_out;
+
   initial begin
     // Let the simulator get caught up before starting
-    #100ns;
+    #100;
 
     // -- Test Addition Operation
     // Testing addition without a carry in
+    tests_failed = 0;
+    tests_passed = 0;
+
     alu_control = ADD;
     alu_carry_in = 1'b0;
 
-    for (A=0; A<256; A=A+1) begin
-      for (B=0; B<256; B=B+1) begin
+    for (A=0; A<256; A=A+1'd1) begin
+      for (B=0; A<256; B=B+1'd1) begin
         alu_AI = A;
         alu_BI = B;
-        #10ns;
+        #10;
 
         // Test carry out with no overflow
         if (!alu_AI[7] && !alu_BI[7] && alu_carry_in) begin
@@ -113,13 +118,13 @@ module alu_tb ();
         // Test carry out
         if (A + B > 255) begin
           if (alu_carry_out == 1'b1) begin
-          tests_passed = tests_passed + 1;
+            tests_passed = tests_passed + 1;
           end else begin
             tests_failed = tests_failed + 1;
           end
         end else begin
           if (alu_carry_out == 1'b0) begin
-          tests_passed = tests_passed + 1;
+            tests_passed = tests_passed + 1;
           end else begin
             tests_failed = tests_failed + 1;
           end
@@ -135,12 +140,42 @@ module alu_tb ();
       $display("Addition...%d passing and %d failures.", tests_passed, tests_failed);
     end
 
-    // -- Testing Right Shift Operation
-    #100ns;
+    #100;
 
+    // -- Testing Right Shift Operation
     tests_failed = 0;
     tests_passed = 0;
 
+    for (A=0; A<256; A=A+1) begin
+      alu_AI = A;
+      alu_control = SR;
+      alu_carry_in = 1'b0;
+      if (A % 2 == 1) begin
+        result = (A - 1) / 2;
+        carry_out = 1'b1;
+      end else begin
+        result = A / 2;
+        carry_out = 1'b1;
+      end
+      #10;
+
+      if (alu_Y == result && carry_out == alu_carry_out) begin
+        tests_passed = tests_passed + 1;
+      end else begin
+        tests_failed = tests_failed + 1;
+      end
+
+      alu_carry_in = 1'b1;
+      #10;
+      if (alu_Y == result + 128 && carry_out == alu_carry_out) begin
+        tests_passed = tests_passed + 1;
+      end else begin
+        tests_failed = tests_failed + 1;
+      end
+
+    end
+
+    $finish;
   end
 
 endmodule // alu_tb
