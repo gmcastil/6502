be89a03bb9b7fc1c481bd5d4ffb4a737fb5218f1
George Castillo <gmcastil@gmail.com>
2017-11-19 13:47:34 -0700

    Altered environment variable definitions for the proc sim.

--- sim/proc_sim.tcl
+++ sim/proc_sim.tcl
@@ -6,11 +6,11 @@ set includes_dir "../src/includes"
 set memory_dir "../build/memory_block"
 set dofiles "./dofiles"
 
-set vlib ${env(QUESTA_BIN)}/vlib
-set vlog ${env(QUESTA_BIN)}/vlog
-set vsim ${env(QUESTA_BIN)}/vsim
+set vlib ${::env(QUESTA_BIN)}/vlib
+set vlog ${::env(QUESTA_BIN)}/vlog
+set vsim ${::env(QUESTA_BIN)}/vsim
 
-set xilinx_vivado ${env(XILINX_VIVADO)}
+set xilinx_vivado ${::env(XILINX_VIVADO)}
 
 $vlib proc_lib
 
