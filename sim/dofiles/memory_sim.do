394a7d7bd1807fefa40e1e07c4fea031eb147d10
George Castillo <gmcastil@gmail.com>
2018-04-25 14:09:32 -0600

    No reason to require SystemVerilog and also fixed an optimization bug in the do file for the memory simulation which different versions of Questa have issues with.

--- sim/dofiles/memory_sim.do
+++ sim/dofiles/memory_sim.do
@@ -1,3 +1,3 @@
-vsim memory_lib.memory_tb
+vsim -novopt memory_lib.memory_tb
 log -r /*
 run -all
--- src/memory.v
+++ src/memory.v
@@ -1,12 +1,18 @@
 // ----------------------------------------------------------------------------
-// Module:  memory.sv
+// Module:  memory.v
 // Project: MOS 6502 Processor
 // Author:  George Castillo <gmcastil@gmail.com>
 // Date:    23 April 2018
 //
 // Description: Behavioral model for a 64kB asynchronous memory. The module
 // reads a text file into a large array and then either reads or writes to the
-// array in a clocked process with a programmable delay.
+// array in a clocked process with a programmable delay.  A reset immediately
+// reloads the contents of the data file provided during compilation.
+//
+// This is not truly an asynchronous memory - if it were, it would respond to
+// things other than just the rising edge of the clock.  For that matter, if
+// it were truly asynchronous, it would not have a clock at all.  The purpose
+// of this module is to model a memory which responds to
 // ----------------------------------------------------------------------------
 `timescale 1ns / 1ps
 
@@ -17,16 +23,18 @@ module memory
     parameter ASYNC_DELAY = 1,
     parameter DATA_FILE = "data.mif"
     ) (
-       input                  clk,
-       input                  resetn,
-       input                  enable,
-       input [DEPTH-1:0]      address,
-       input                  wr_enable,
-       input [WIDTH-1:0]      wr_data,
-       output reg [WIDTH-1:0] rd_data
+       input wire             clk,
+       input wire             resetn,
+       input wire             enable,
+       input wire [(DEPTH-1):0] address,
+       input wire             wr_enable,
+       input wire [(WIDTH-1):0] wr_data,
+       output reg [(WIDTH-1):0] rd_data
        );
 
-  logic [WIDTH-1:0] mem_array [0:(2**DEPTH)-1];
+  localparam DISABLED = 8'hff;
+
+  reg [(WIDTH-1):0] mem_array [0:(2**DEPTH)-1];
   initial begin
     $readmemb(DATA_FILE, mem_array);
   end
@@ -43,6 +51,8 @@ module memory
       mem_array[address] <= wr_data;
     end else if ( enable ) begin
       rd_data <= mem_array[address];
+    end else begin
+      rd_data <= DISABLED;
     end
   end
 
