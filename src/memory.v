10270bd1a86abd80d8c4c9a3fd91aa816106ed4b
George Castillo <gmcastil@gmail.com>
2018-04-25 14:16:31 -0600

    Finished summary of memory.v and some formatting changes to the module.

--- src/memory.v
+++ src/memory.v
@@ -12,39 +12,43 @@
 // This is not truly an asynchronous memory - if it were, it would respond to
 // things other than just the rising edge of the clock.  For that matter, if
 // it were truly asynchronous, it would not have a clock at all.  The purpose
-// of this module is to model a memory which responds to
+// of this module is to model a memory which accepts an address on the rising
+// edge of a clock and provides the data a configurable amount of time later,
+// like an asynchronous RAM would.
 // ----------------------------------------------------------------------------
+
 `timescale 1ns / 1ps
 
-module memory
-  #(
-    parameter DEPTH = 16,
-    parameter WIDTH = 8,
-    parameter ASYNC_DELAY = 1,
-    parameter DATA_FILE = "data.mif"
-    ) (
-       input wire             clk,
-       input wire             resetn,
-       input wire             enable,
-       input wire [(DEPTH-1):0] address,
-       input wire             wr_enable,
-       input wire [(WIDTH-1):0] wr_data,
-       output reg [(WIDTH-1):0] rd_data
-       );
+module memory #(
+     parameter DEPTH = 16,
+     parameter WIDTH = 8,
+     parameter ASYNC_DELAY = 1,
+     parameter DATA_FILE = "data.mif"
+  ) (
+     input wire               clk,
+     input wire               resetn,
+     input wire               enable,
+     input wire [(DEPTH-1):0] address,
+     input wire               wr_enable,
+     input wire [(WIDTH-1):0] wr_data,
+     output reg [(WIDTH-1):0] rd_data
+     );
 
   localparam DISABLED = 8'hff;
 
-  reg [(WIDTH-1):0] mem_array [0:(2**DEPTH)-1];
+  reg [(WIDTH-1):0]           mem_array [0:(2**DEPTH)-1];
   initial begin
     $readmemb(DATA_FILE, mem_array);
   end
 
+  // -- Reset logic
   always @(posedge clk) begin
     if ( resetn == 1'b0 ) begin
       $readmemb(DATA_FILE, mem_array);
     end
   end
 
+  // -- Memory model
   always @(posedge clk) begin
     #ASYNC_DELAY;
     if ( wr_enable && enable ) begin
